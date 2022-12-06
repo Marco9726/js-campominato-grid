eec7d9345feebfe5faf2757a9da0e86420383c27
Marco 9726 <marcosarra9@gmail.com>
2022-12-06 15:14:17 +0100

    verifica funzionamento eventListner al click del bottone

---
+++ js/script.js
@@ -0,0 +1,6 @@
+    //1.1 - creo variabile collegata al bottone
+const button = document.getElementById('button')
+//2 - addEventListner al click del bottone
+button.addEventListener('click', function () {
+    alert('ciao')
+})
