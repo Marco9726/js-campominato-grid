744215712bd0d95e9243b7b7d203c0daf37d4031
Marco 9726 <marcosarra9@gmail.com>
2022-12-06 17:19:41 +0100

    - visualizzo il numero dell'iterazione nel suo square

--- js/script.js
+++ js/script.js
@@ -9,18 +9,21 @@
 //3 - addEventListner al click del bottone
 button.addEventListener('click', function () {
     //3.1 - creo ciclo for per generare 100 square
-    for (let i = 0; i < 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         //3.1.1 - creo l'elemento div all'interno della grid
         square = grid.appendChild(document.createElement('div'));
         console.log(square);
         //3.1.2 - assegno classe square al div creato
         square.className = 'square';
-        //3.1.3 - addEventListner al click dello square
+        //3.1.3 - visualizzo il numero dell'iterazione nel suo square
+        square.append(i)
+        //3.1.4 - addEventListner al click dello square
         square.addEventListener('click', function() {
-            //3.1.3.1- assegno classe bg-blue allo square
+            //3.1.4.1- assegno classe bg-blue allo square
             this.classList.add('bg-blue')
-            //3.1.3.2 - visualizzo numero dello suqare cliccato in console log
+            //3.1.4.2 - visualizzo numero dello suqare cliccato in console log
             console.log(i);
         }) 
     }
+      
 })
